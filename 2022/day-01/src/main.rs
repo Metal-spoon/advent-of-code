b0125db6a4f7de01762131b98cd1de01eefcba1a
Metal-spoon <9281807+Metal-spoon@users.noreply.github.com>
2022-12-07 08:00:57 +0100

    Day 1 part 2 done

--- 2022/day-01/src/main.rs
+++ 2022/day-01/src/main.rs
@@ -1,26 +1,28 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::vec;
 
 
 fn main() {
     println!("Advent of code day one");
     let mut current_segment: i32 = 0;
-    let mut max_segment: i32 = 0;
+    let mut segments: Vec<i32> = Vec::new();
     if let Ok(lines) = read_lines("input.txt") {
         for line in lines {
             if let Ok(text) = line {
                 if text == "" {
-                    if current_segment > max_segment {
-                        max_segment = current_segment;
-                    }
+                    segments.push(current_segment);
                     current_segment = 0;
                 } else {
                     current_segment += text.trim().parse::<i32>().expect("please give a valid number");
             }
             }
         }
-        println!("{}", max_segment);
+        segments.sort_by(|a, b| b.cmp(a));
+        let top_3_sum: i32 = segments.iter().take(3).sum();
+        println!("Top segment: {}", segments[0]);
+        println!("Sum of top 3: {}", top_3_sum)
     }
 }
 
